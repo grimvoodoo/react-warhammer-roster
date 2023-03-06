48b3beddcef78bfc4a33d972bc1d00b5b82a7fd7
acleveland <andrew.cleveland@hotmail.co.uk>
2023-03-06 23:00:45 +0000

    AC - got it working

--- src/components/armyList/ArmyUnit.tsx
+++ src/components/armyList/ArmyUnit.tsx
@@ -1,22 +1,25 @@
 import { ArmyWeapons } from "./ArmyWeapons";
 export const ArmyUnit = (props: any) => {
     const clickHandler = () => {
-        console.log(props.id);
+        console.log(props);
     };
 
     return (
-        // <div className="p-6 max-w-sm mx-auto bg-slate-700 text-white rounded-xl shadow-lg flex items-center space-x-4">
-        <div className="mx-auto relative container mx-auto bg-slate-700 text-slate-200 p-6 rounded-3xl m-3 flex justify-center overflow-hidden">
-            <div className="  ">
-                <h2>{props.title}</h2>
-                <div>
-                    {props.weapons.map((weapon: any) => (
-                        <ArmyWeapons weapons={weapon} />
-                    ))}
+        <button
+            className="mx-auto relative container mx-auto bg-slate-700 text-slate-200 p-6 rounded-3xl m-3 overflow-hidden"
+            onClick={clickHandler}
+        >
+            <div>
+                <div className="flex items-center justify-between">
+                    <h2 className="">{props.title}</h2>
+                    <div className="">
+                        {props.weapons.map((weapon: any) => (
+                            <ArmyWeapons weapons={weapon} />
+                        ))}
+                    </div>
+                    <div className="">Points: {props.amount}</div>
                 </div>
-                <div className="unit-cost">Points: {props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Add</button>
-        </div>
+        </button>
     );
 };
